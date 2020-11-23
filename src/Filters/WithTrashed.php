74bb1e1cae4aa6cd2e45eb49dbcaa60445edfc4e
tabuna <bliz48rus@gmail.com>
2020-11-23 15:07:02 +0300

    Added with trashed filter

---
+++ src/Filters/WithTrashed.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Orchid\Crud\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+use Orchid\Filters\Filter;
+use Orchid\Screen\Field;
+use Orchid\Screen\Fields\CheckBox;
+
+class WithTrashed extends Filter
+{
+    /**
+     * @var array
+     */
+    public $parameters = [
+        'withTrashed'
+    ];
+
+    /**
+     * @return string
+     */
+    public function name(): string
+    {
+        return 'Deleted entries';
+    }
+
+    /**
+     * @param Builder $builder
+     *
+     * @return Builder
+     */
+    public function run(Builder $builder): Builder
+    {
+        return $builder->onlyTrashed();
+    }
+
+    /**
+     * @return Field[]
+     */
+    public function display(): array
+    {
+        return [
+            CheckBox::make('withTrashed')
+                ->placeholder(__('Show deleted entries'))
+        ];
+    }
+}
