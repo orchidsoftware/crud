431e6b9cd2711ddf6b88fe13fb32beff68a7ef33
tabuna <bliz48rus@gmail.com>
2020-12-12 18:00:57 +0300

    Added tests for usage authorization user

--- tests/CrudPolicyTest.php
+++ tests/CrudPolicyTest.php
@@ -3,9 +3,12 @@
 namespace Orchid\Crud\Tests;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Hash;
 use Orchid\Crud\Tests\Fixtures\PostResource;
 use Orchid\Crud\Tests\Models\Post;
 use Orchid\Crud\Tests\Policies\PostProtectedPolicy;
+use Orchid\Platform\Models\User;
+use Orchid\Support\Facades\Dashboard;
 
 class CrudPolicyTest extends TestCase
 {
@@ -17,7 +20,25 @@ class CrudPolicyTest extends TestCase
         parent::setUp();
 
         Gate::policy(Post::class, PostProtectedPolicy::class);
+
+        $this->loginWithFakeUser();
     }
+
+    /**
+     *
+     */
+    public function loginWithFakeUser()
+    {
+        $user = new User([
+            'name'     => 'Admin',
+            'email'    => 'admin@admin.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $user->permissions = Dashboard::getAllowAllPermission();
+        $this->be($user);
+    }
+
 
     /**
      *
