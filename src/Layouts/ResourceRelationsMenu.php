51ce6c51b04dfecdc80820edacffc7053246453b
bald-cat <babaiev.ihor@gmail.com>
2025-04-04 18:15:41 +0300

    return [] when count < 2

--- src/Layouts/ResourceRelationsMenu.php
+++ src/Layouts/ResourceRelationsMenu.php
@@ -2,29 +2,30 @@
 
 namespace Orchid\Crud\Layouts;
 
+use Illuminate\Support\Collection;
 use Orchid\Screen\Actions\Menu;
 use Orchid\Screen\Layouts\TabMenu;
 
 class ResourceRelationsMenu extends TabMenu {
 
     public function __construct(
-        private array $relations
-    ) {}
+        private Collection $relations,
+    ) {
+        $this->relations = collect($this->relations);
+    }
 
     public static function make(array $relations): static
     {
-        return new static($relations);
+        return new static(collect($relations));
     }
 
     protected function navigations(): iterable {
 
-        $menu = [];
-
-        foreach ($this->relations as $resource) {
-            $menu[] = Menu::make($resource->label());
-        }
-
-        return $menu;
+        return $this->relations->count() < 2
+            ? []
+            : $this->relations->map(
+                fn($resource) => Menu::make($resource->label())
+            );
     }
 
 }
