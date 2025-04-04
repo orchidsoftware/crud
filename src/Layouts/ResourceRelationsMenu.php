0a94d05aed2980d43dc0d9e5c3afa47d96feaa4a
bald-cat <babaiev.ihor@gmail.com>
2025-04-04 19:11:45 +0300

    changed menu, added class active

--- src/Layouts/ResourceRelationsMenu.php
+++ src/Layouts/ResourceRelationsMenu.php
@@ -20,12 +20,29 @@ class ResourceRelationsMenu extends TabMenu {
     }
 
     protected function navigations(): iterable {
+        if ($this->relations->count() < 2) {
+            return [];
+        }
 
-        return $this->relations->count() < 2
-            ? []
-            : $this->relations->map(
-                fn($resource) => Menu::make($resource->label())
-            );
+        $currentRelation = request('relation');
+        $firstKey = $this->relations->keys()->first();
+
+        return $this->relations->map(function ($resource, $key) use ($currentRelation, $firstKey) {
+            $menu = Menu::make($resource->label())
+                ->route('platform.resource.view', [
+                    'resource' => request('resource'),
+                    'id' => request('id'),
+                    'relation' => $key,
+                ]);
+
+            if ($currentRelation === null ? $key === $firstKey : $key === $currentRelation) {
+                $menu->addClass('active');
+            }
+
+            return $menu;
+        })->toArray();
     }
+
+
 
 }
